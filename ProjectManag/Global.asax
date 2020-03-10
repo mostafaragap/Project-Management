89b67fbe45ed87cf54e497ba5bd2e9488e18fa00
mostafa ragap <mostafaragap350@gmail.com>
2020-03-10 16:31:38 +0200

    Creating project and setup files for design and home page design

---
+++ ProjectManag/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectManag.MvcApplication" Language="C#" %>
---
+++ ProjectManag/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectManag.Startup))]
+namespace ProjectManag
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
